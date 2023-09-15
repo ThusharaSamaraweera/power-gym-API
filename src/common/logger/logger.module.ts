067147ec3041df7b8afb06b94da52574fecdb13c
ThusharaSamaraweera <thusharadasun204@gmail.com>
2023-09-16 00:47:55 +0530

    add pino-pretty logger

---
+++ src/common/logger/logger.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { LoggerModule as PinoLoggerModule } from 'nestjs-pino';
+
+@Module({
+  imports: [
+    PinoLoggerModule.forRoot({
+      pinoHttp: {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            singleLine: true,
+          },
+        },
+      },
+    }),
+  ],
+  exports: [PinoLoggerModule],
+})
+export class LoggerModule {}
