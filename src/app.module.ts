0b819f87b68c608a365902f34026011eb8521565
ThusharaSamaraweera <thusharadasun204@gmail.com>
2023-09-16 14:41:51 +0530

    add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerModule } from './common';
+import { ConfigModule } from './common/config/config.module';
 
 @Module({
-  imports: [LoggerModule],
+  imports: [LoggerModule, ConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from 'nestjs-pino';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'debug', 'verbose', 'log'],
+  });
+  app.enableCors();
   app.useLogger(app.get(Logger));
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // This will remove any properties that are not in the DTO
+      transform: true, // This will transform the incoming data to the DTO type
+      transformOptions: {
+        enableImplicitConversion: true, // This will convert the incoming data to the type specified in the DTO
+      },
+    }),
+  );
+  const port = process.env.PORT || 8080;
+  await app.listen(port, () => {
+    console.log(`🚀 Listening at http://localhost:${port}`);
+  });
 }
 bootstrap();
