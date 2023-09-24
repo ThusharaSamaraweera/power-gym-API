49fba7bec40d4b1a452d262fec9e642870f66a35
ThusharaSamaraweera <thusharadasun204@gmail.com>
2023-09-24 16:21:00 +0530

    update BodyHealthInfoSchema and add its dto

---
+++ src/user/dto/body-health-info.dto.ts
@@ -0,0 +1,11 @@
+export class BodyInfoDto {
+  height: number;
+  weight: number;
+  fat: number;
+  muscleMass?: number;
+}
+
+export class HealthInfoDto {
+  bloodPressure: string;
+  heartRate: number;
+}
--- src/user/types.ts
+++ src/user/types.ts
@@ -1,11 +1,11 @@
-export interface BodyInfo {
-  height: number;
-  weight: number;
-  fat: number;
-  muscleMass: number;
-}
+// export interface BodyInfo {
+//   height: number;
+//   weight: number;
+//   fat: number;
+//   muscleMass?: number;
+// }
 
-export interface HealthInfo {
-  bloodPressure: string;
-  heartRate: number;
-}
+// export interface HealthInfo {
+//   bloodPressure: string;
+//   heartRate: number;
+// }
