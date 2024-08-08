560864ab0a4b96c20a5e3fa0de2476b49784874b
ThusharaSamaraweera <thusharadasun204@gmail.com>
2024-08-08 21:57:50 +0530

    get trainer id from user on body health info creation

--- src/user/bodyHealthInfo.service.ts
+++ src/user/bodyHealthInfo.service.ts
@@ -3,24 +3,33 @@ import { CreateBodyHealthInfoDto } from './dto/create-body-health-info-dto';
 import { Injectable } from '@nestjs/common';
 import { BodyHealthInfoRepository } from './repository/body-health-info.repository';
 import { BODY_HEALTH_INFO_RECORD_STATUS } from './types';
+import { UserRepository } from './repository/user.repository';
 
 @Injectable()
 export class BodyHealthInfoService {
   constructor(
     private readonly bodyHealthInfoRepository: BodyHealthInfoRepository,
+    private readonly userRepository: UserRepository,
   ) {}
 
   async createBodyHealthInfo(
     bodyHealthInfo: CreateBodyHealthInfoDto,
     memberId: Types.ObjectId,
   ) {
+    const user = await this.userRepository.findOne({
+      _id: memberId,
+    });
+    console.log(
+      '🚀 ~ file: bodyHealthInfo.service.ts:27 ~ BodyHealthInfoService ~ trainerId:',
+      user,
+    );
     return await this.bodyHealthInfoRepository.create({
       memberId: memberId,
       bodyHealthInfo: bodyHealthInfo.bodyHealthInfo,
       note: bodyHealthInfo?.note,
       verifiedBy: bodyHealthInfo?.verifiedBy,
       status: bodyHealthInfo?.status || BODY_HEALTH_INFO_RECORD_STATUS.PENDING,
-      trainerId: bodyHealthInfo.trainerId,
+      trainerId: user?.trainerId,
       WorkoutPlan: bodyHealthInfo?.workoutPlan,
     });
   }
